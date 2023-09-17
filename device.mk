3c11719790b75813474545bc844f90f96f814bc4
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-17 12:43:53 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ ENABLE_VIRTUAL_AB := true
   
  AB_OTA_POSTINSTALL_CONFIG += \ 
      RUN_POSTINSTALL_system=true \ 
-     POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \ 
+     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
      FILESYSTEM_TYPE_system=erofs \ 
      POSTINSTALL_OPTIONAL_system=true  
   
