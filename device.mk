8d4168794191ab9b4a272191fea8c3fc6f9ccbbc
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-17 10:45:31 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -22,48 +22,56 @@ AB_OTA_PARTITIONS += \
     vbmeta \
     vbmeta_system \
     vbmeta_vendor
-
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+ 
   
- # HIDL 
- PRODUCT_PACKAGES += \ 
-     android.hidl.base@1.0_system \ 
-     android.hidl.manager@1.0_system 
+ LOCAL_PATH := device/xiaomi/ruby 
+ # A/B 
+ AB_OTA_POSTINSTALL_CONFIG += \ 
+     RUN_POSTINSTALL_system=true \ 
+     POSTINSTALL_PATH_system=system/bin/otapreopt_script \ 
+     FILESYSTEM_TYPE_system=erofs \ 
+     POSTINSTALL_OPTIONAL_system=true 
   
- # Health 
+ AB_OTA_POSTINSTALL_CONFIG += \ 
+     RUN_POSTINSTALL_system=true \ 
+     POSTINSTALL_PATH_system=system/bin/ mtk_plpath_utils \
+     FILESYSTEM_TYPE_system=erofs \ 
+     POSTINSTALL_OPTIONAL_system=true 
+
+ # Boot control HAL 
  PRODUCT_PACKAGES += \ 
-     android.hardware.health@2.1-impl \ 
-     android.hardware.health@2.1-service
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl.recovery \
-
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
-
-# Additional binaries & libraries needed for recovery
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libkeymaster4 \
-    libpuresoftkeymasterdevice
-
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+     android.hardware.boot@1.1-impl \ 
+     android.hardware.boot@1.1-impl.recovery \ 
+     android.hardware.boot@1.2-mtkimpl.recovery 
+  
+ PRODUCT_PACKAGES += \ 
+     bootctrl.mt6877 \ 
+     bootctrl.mt6877.recovery 
+  
+ PRODUCT_PACKAGES_DEBUG += \ 
+     bootctl 
+  
+ PRODUCT_PACKAGES += \ 
+     otapreopt_script \ 
+     cppreopts.sh \ 
+     update_engine \ 
+     update_verifier \ 
+     update_engine_sideload 
+  
+ # Keymaster 
+ TARGET_RECOVERY_DEVICE_MODULES += \ 
+     libkeymaster4 \ 
+     libkeymaster41 \ 
+     libpuresoftkeymasterdevice 
+  
+ RECOVERY_LIBRARY_SOURCE_FILES += \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so 
+  
+ # API 
+ PRODUCT_SHIPPING_API_LEVEL := 31 
+  
+ # VNDK
 
 
