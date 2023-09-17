afc24c1a133f28a402faf720802954ca43b8ab1a
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-17 12:41:55 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,54 +4,45 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
-# A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    system \
-    system_ext \
-    product \
-    vendor \
-    odm \
-    vbmeta \
-    vbmeta_system \
-    vbmeta_vendor
  
+ENABLE_VIRTUAL_AB := true 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk) 
   
- LOCAL_PATH := device/xiaomi/ruby 
  # A/B 
+ AB_OTA_UPDATER := true 
+ AB_OTA_PARTITIONS += \ 
+     system \
+     product \
+     vendor \
+     vbmeta_system \ 
+     vbmeta_vendor
+  
  AB_OTA_POSTINSTALL_CONFIG += \ 
      RUN_POSTINSTALL_system=true \ 
-     POSTINSTALL_PATH_system=system/bin/otapreopt_script \ 
+     POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \ 
      FILESYSTEM_TYPE_system=erofs \ 
      POSTINSTALL_OPTIONAL_system=true  
-
+  
  # Boot control HAL 
  PRODUCT_PACKAGES += \ 
-     android.hardware.boot@1.1-impl \ 
-     android.hardware.boot@1.1-impl.recovery \ 
      android.hardware.boot@1.2-mtkimpl.recovery 
   
+ # Fastbootd 
  PRODUCT_PACKAGES += \ 
-     bootctrl.mt6877 \ 
-     bootctrl.mt6877.recovery 
+     android.hardware.fastboot@1.0-impl-mock \ 
+     fastbootd 
   
  PRODUCT_PACKAGES_DEBUG += \ 
-     bootctl 
+     update_engine_client 
   
  PRODUCT_PACKAGES += \ 
      otapreopt_script \ 
      cppreopts.sh \ 
      update_engine \ 
      update_verifier \ 
-     update_engine_sideload 
-  
- # Keymaster 
+     update_engine_sideload
+
+# Keymaster 
  TARGET_RECOVERY_DEVICE_MODULES += \ 
      libkeymaster4 \ 
      libkeymaster41 \ 
@@ -60,9 +51,9 @@ AB_OTA_PARTITIONS += \
  RECOVERY_LIBRARY_SOURCE_FILES += \ 
      $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \ 
      $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \ 
-     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so 
-  
- # API 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
+  # API 
  PRODUCT_SHIPPING_API_LEVEL := 31 
   
  # VNDK
