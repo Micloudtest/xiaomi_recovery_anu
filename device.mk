d18e18238e5611e8b9ab02131665b3f6cc43f536
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-17 14:23:02 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,13 +4,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
- 
-ENABLE_VIRTUAL_AB := true 
- $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk) 
-  
- # A/B 
- AB_OTA_UPDATER := true 
- AB_OTA_PARTITIONS += \ 
+
+
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
     boot \
     dtbo \
     system \
@@ -21,44 +22,44 @@ ENABLE_VIRTUAL_AB := true
     vbmeta \
     vbmeta_system \
     vbmeta_vendor
-  
- AB_OTA_POSTINSTALL_CONFIG += \ 
-     RUN_POSTINSTALL_system=true \ 
-     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-     FILESYSTEM_TYPE_system=erofs \ 
-     POSTINSTALL_OPTIONAL_system=true  
-  
-  PRODUCT_PACKAGES += \ 
-     otapreopt_script \ 
-     cppreopts.sh \
 
- # Boot control HAL 
- PRODUCT_PACKAGES += \ 
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh 
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hardware.boot@1.2-impl \
     android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.2-service
-  
- # Fastbootd 
- PRODUCT_PACKAGES += \ 
-     android.hardware.fastboot@1.0-impl-mock \ 
-     fastbootd  
-  
- PRODUCT_PACKAGES += \ 
-     update_engine \ 
-     update_verifier \ 
-     update_engine_sideload
 
- PRODUCT_PACKAGES_DEBUG += \ 
-     bootctl
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
-# Keymaster 
- TARGET_RECOVERY_DEVICE_MODULES += \ 
-     libkeymaster4 \ 
-     libkeymaster41 \ 
-     libpuresoftkeymasterdevice 
-  
- RECOVERY_LIBRARY_SOURCE_FILES += \ 
-     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \ 
-     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \ 
-     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libkeymaster41 \
+    libpuresoftkeymasterdevice
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
+
