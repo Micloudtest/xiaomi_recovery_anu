681ad1d84acbdc975920b8cc2f02e0f7838cbebc
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-18 12:49:26 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,50 +1,36 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
-# A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    boot \
-    system \
-    product \
-    vendor \
-    vbmeta_system \
-    vbmeta_vendor
-
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh 
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
+# 
+ # Copyright (C) 2022 The Android Open Source Project 
+ # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator 
+ # 
+ # SPDX-License-Identifier: Apache-2.0 
+ # 
+  
+ 
+ # A/B 
+ AB_OTA_POSTINSTALL_CONFIG += \ 
+     RUN_POSTINSTALL_system=true \ 
+     POSTINSTALL_PATH_system=system/bin/otapreopt_script \ 
+     FILESYSTEM_TYPE_system=ext4 \ 
+     POSTINSTALL_OPTIONAL_system=true 
+  
+ # Boot control HAL 
+ PRODUCT_PACKAGES += \ 
+     android.hidl.base@1.0 \
     android.hardware.boot@1.2-impl \
     android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.2-service
-
+   
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
-
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
+  
+ PRODUCT_PACKAGES += \ 
+     otapreopt_script \ 
+     cppreopts.sh \ 
+     update_engine \ 
+     update_verifier \ 
+     update_engine_sideload
 
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
@@ -56,5 +42,3 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-
-
