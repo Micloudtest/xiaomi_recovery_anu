93e53f7d5c7376e3e7b221cd0ff6e68beb528f2a
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-18 20:25:36 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -72,11 +72,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek
 
-# Additional target Libraries
+# Keymaster
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
+    libkeymaster41 \
     libpuresoftkeymasterdevice
 
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
