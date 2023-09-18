5c3f833a9362d9c694bdff71ee84f81b932255ba
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-18 14:48:57 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,13 +1,10 @@
-# 
- # Copyright (C) 2022 The Android Open Source Project 
- # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator 
- # 
- # SPDX-License-Identifier: Apache-2.0 
- # 
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-# For building with minimal manifest 
- ALLOW_MISSING_DEPENDENCIES := true 
-  
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
@@ -15,46 +12,48 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
     boot \
-    product \
     system \
+    product \
     vendor \
     vbmeta_system \
     vbmeta_vendor
+
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
     
- 
- # A/B 
- AB_OTA_POSTINSTALL_CONFIG += \ 
-     RUN_POSTINSTALL_system=true \ 
-     POSTINSTALL_PATH_system=system/bin/otapreopt_script \ 
-     FILESYSTEM_TYPE_system=ext4 \ 
-     POSTINSTALL_OPTIONAL_system=true 
-  
- # Boot control HAL 
- PRODUCT_PACKAGES += \ 
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hardware.boot@1.1-impl \ 
-    android.hardware.boot@1.1-impl.recovery \ 
-    android.hardware.boot@1.2-mtkimpl.recovery
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
 
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
-  
-# Keymaster 
- TARGET_RECOVERY_DEVICE_MODULES += \ 
-     libkeymaster4 \ 
-     libkeymaster41 \ 
-     libpuresoftkeymasterdevice 
-  
- RECOVERY_LIBRARY_SOURCE_FILES += \ 
-     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \ 
-     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \ 
-     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-   
- PRODUCT_PACKAGES += \ 
-     otapreopt_script \ 
-     cppreopts.sh \ 
-     update_engine \ 
-     update_verifier \ 
-     update_engine_sideload
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh 
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libkeymaster41 \
+    libpuresoftkeymasterdevice
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
+
