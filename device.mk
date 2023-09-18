a7b44d1ad01cfce7c6117fa7ca33ad204ecad3cb
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-18 16:17:05 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -28,7 +28,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
     android.hardware.boot@1.2-impl \
     android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.2-service
