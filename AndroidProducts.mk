d15a3c91eac8f41cc2a72343bd6e14e0244ea89d
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-19 16:33:24 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,4 +8,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_ruby.mk
 
 COMMON_LUNCH_CHOICES := \
+
+    twrp_ruby-user \
+    twrp_ruby-userdebug \
     twrp_ruby-eng
