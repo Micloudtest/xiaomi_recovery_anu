b1830d69e3408051d16995fd430e38d327efa4ae
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-19 16:25:57 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,5 +7,5 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),ruby)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
