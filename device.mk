077e1dba9c0f7438a98e8ba394e31f26028d0497
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-19 12:23:05 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -65,6 +65,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
 
 PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
 # Gatekeeper
