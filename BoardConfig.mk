74fa95edf8fdea52ec8f920a5a5a43bef8328889
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-19 18:01:07 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ AB_OTA_PARTITIONS := \
     vbmeta_system \
     vbmeta_vendor
 
-# Architecture
+# Architecture 
  TARGET_ARCH := arm64 
  TARGET_ARCH_VARIANT := armv8-a 
  TARGET_CPU_ABI := arm64-v8a 
@@ -34,11 +34,11 @@ AB_OTA_PARTITIONS := \
  TARGET_CPU_VARIANT_RUNTIME := cortex-a55 
   
  TARGET_2ND_ARCH := arm 
- TARGET_2ND_ARCH_VARIANT := armv7-a
+ TARGET_2ND_ARCH_VARIANT := armv7-a-neon 
  TARGET_2ND_CPU_ABI := armeabi-v7a 
  TARGET_2ND_CPU_ABI2 := armeabi 
  TARGET_2ND_CPU_VARIANT := generic 
- TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55 
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := ruby
