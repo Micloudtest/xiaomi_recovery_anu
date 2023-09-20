cfe65944c93caaebcd4811db16bf6738260024fe
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2023-09-20 18:14:17 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,20 +4,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Dynamic Partitions 
+ PRODUCT_USE_DYNAMIC_PARTITIONS := true 
+  
+ # API 
+ PRODUCT_SHIPPING_API_LEVEL := 30 
+  
+ # Virtual A/B 
+ ENABLE_VIRTUAL_AB := true 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk) 
+  
+ # Enable project quotas and casefolding for emulated storage without sdcardfs 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk) 
+  
+ # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot. 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # A/B
-# Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
-
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
+    otapreopt_script
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -25,15 +30,20 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=erofs \
-    POSTINSTALL_OPTIONAL_vendor=true
-
+# Boot control HAL
 PRODUCT_PACKAGES += \
-    checkpoint_gc \
-    otapreopt_script
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    bootctrl.mt6877.recovery
+
+# Update Engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
