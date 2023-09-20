ce5bab0d2156dac8484b656e0853beb8570a1e92
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2023-09-21 01:52:36 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,45 +1,117 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+ # 
+ # Copyright (C) 2023 The Android Open Source Project 
+ # 
+ # SPDX-License-Identifier: Apache-2.0 
+ # 
+  
+ # Configure Virtual A/B 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk) 
+  
+ # Enable updating of APEXes 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk) 
+  
+ # Enable developer GSI keys 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk) 
+  
+ # Configure emulated_storage.mk 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk) 
+  
+ # Building with minimal manifest 
+ ALLOW_MISSING_DEPENDENCIES := true 
+  
+ # Boot control, Firmware 
+ PRODUCT_PACKAGES += \ 
+     android.hardware.boot@1.2-impl.recovery \ 
+     miui_prebuilt 
+  
+ # FastbootD support 
+ PRODUCT_PACKAGES += \ 
+     android.hardware.fastboot@1.1-impl-mock \ 
+     fastbootd 
+  
+ # Update engine 
+ PRODUCT_PACKAGES += \ 
+     update_engine \ 
+     update_engine_sideload \ 
+     update_verifier 
+  
+ PRODUCT_PACKAGES_DEBUG += \ 
+     update_engine_client 
+  
+ PRODUCT_PACKAGES += \ 
+     otapreopt_script \ 
+     checkpoint_gc 
+  
+ # API 
+ PRODUCT_SHIPPING_API_LEVEL  := 31 
+ PRODUCT_TARGET_VNDK_VERSION := 33 
+  
+ # Display Size & Density 
+ TARGET_SCREEN_HEIGHT  := 2400 
+ TARGET_SCREEN_DENSITY := 390 
+ TARGET_SCREEN_WIDTH   := 1080 
+  
+ # Dynamic partitions 
+ PRODUCT_USE_DYNAMIC_PARTITIONS := true 
+ PRODUCT_BUILD_SUPER_PARTITION  := false 
+  
+ # Virutal A/B 
+ ENABLE_VIRTUAL_AB := true 
+ AB_OTA_POSTINSTALL_CONFIG += \ 
+     RUN_POSTINSTALL_system=true \ 
+     POSTINSTALL_PATH_system=system/bin/otapreopt_script \ 
+     FILESYSTEM_TYPE_system=erofs \ 
+     POSTINSTALL_OPTIONAL_system=true 
+  
+ AB_OTA_POSTINSTALL_CONFIG += \ 
+     RUN_POSTINSTALL_vendor=true \ 
+     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \ 
+     FILESYSTEM_TYPE_vendor=erofs \ 
+     POSTINSTALL_OPTIONAL_vendor=true 
+  
+ PRODUCT_EXTRA_RECOVERY_KEYS += \ 
+     vendor/recovery/security/miui 
+  
+ # Soong namespaces 
+ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH) 
+  
+ # TWRP - Specifics 
+ TW_THEME                := portrait_hdpi 
+ TW_DEFAULT_LANGUAGE     := en 
+ TW_USE_TOOLBOX          := true 
+ TW_INCLUDE_NTFS_3G      := true 
+ TW_INCLUDE_FUSE_EXFAT   := true 
+ TW_INCLUDE_FUSE_NTFS    := true 
+ TW_INCLUDE_REPACKTOOLS  := true 
+ TW_INCLUDE_RESETPROP    := true 
+ TW_INCLUDE_LIBRESETPROP := true 
+ TW_MAX_BRIGHTNESS       := 2047 
+ TW_EXTRA_LANGUAGES      := true 
+ TW_DEFAULT_BRIGHTNESS   := 200 
+ TW_EXCLUDE_APEX         := true 
+ TW_HAS_EDL_MODE         := true 
+ TW_INCLUDE_FASTBOOTD    := true 
+ TWRP_INCLUDE_LOGCAT     := true 
+  
+ # Set panel refresh rate to 90 Hertz for smoother experience 
+ # Possible supported frequencies: 30, 60, 90, 120 
+ TW_FRAMERATE := 90 
+  
+ TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone7/temp" 
+ TW_BRIGHTNESS_PATH      := "/sys/class/backlight/panel0-backlight/brightness" 
 
-# A/B
-PRODUCT_PACKAGES += \
-    otapreopt_script
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    bootctrl.mt6877.recovery
-
-# Update Engine
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
-
-# Keymaster
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libkeymaster4 \
-    libkeymaster41 \
-    libpuresoftkeymasterdevice
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+ TW_BATTERY_SYSFS_WAIT_SECONDS := 6 
+ TW_EXCLUDE_DEFAULT_USB_INIT   := true 
+ TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true 
+  
+ # TWRP - Crypto 
+ TW_INCLUDE_CRYPTO               := true 
+ TW_INCLUDE_CRYPTO_FBE           := true 
+ TW_INCLUDE_FBE_METADATA_DECRYPT := true 
+ BOARD_USES_QCOM_FBE_DECRYPTION  := true 
+ PLATFORM_VERSION                := 99.87.36 
+ PLATFORM_VERSION_LAST_STABLE    := $(PLATFORM_VERSION) 
+  
+ PLATFORM_SECURITY_PATCH := 2127-12-31 
+ VENDOR_SECURITY_PATCH   := $(PLATFORM_SECURITY_PATCH) 
+ BOOT_SECURITY_PATCH     := $(PLATFORM_SECURITY_PATCH)
