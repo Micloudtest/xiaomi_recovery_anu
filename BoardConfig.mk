95af1a2ec57b3e0407958bb3bb4d0c634fbb39d7
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2023-09-20 18:50:10 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,22 +10,6 @@ PREBUILT_PATH := $(DEVICE_PATH)/prebuilt
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
-# A/B
-AB_OTA_UPDATER := true
-TW_INCLUDE_REPACKTOOLS := true
-
-AB_OTA_PARTITIONS := \
-    boot \
-    dtbo \
-    system \
-    system_ext \
-    product \
-    vendor \
-    vbmeta \
-    vbmeta_system \
-    vbmeta_vendor
-BOARD_USES_RECOVERY_AS_BOOT := true 
-
 # Architecture 
  TARGET_ARCH := arm64 
  TARGET_ARCH_VARIANT := armv8-a 
@@ -41,41 +25,78 @@ BOARD_USES_RECOVERY_AS_BOOT := true
  TARGET_2ND_CPU_VARIANT := generic 
  TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55 
 
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := ruby
-TARGET_NO_BOOTLOADER := true
+# A/B
+AB_OTA_UPDATER := true
+TW_INCLUDE_REPACKTOOLS := true
+
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    system \
+    system_ext \
+    product \
+    vendor \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor
+
+# Bootloader 
+ TARGET_BOOTLOADER_BOARD_NAME := ruby
+ TARGET_NO_BOOTLOADER := true 
+ TARGET_USES_UEFI := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6877
+
+# Assert 
+TARGET_OTA_ASSERT_DEVICE := ruby,rubypro
 
 # Kernel
+BOARD_KERNEL_CMDLINE := \
+      bootopt=64S3,32N2,64N2
+
+TARGET_PREBUILT_KERNEL := $(PREBUILT_PATH)/kernel
+TARGET_PREBUILT_DTB := $(PREBUILT_PATH)/dtb.img
+
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x40078000
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x11088000
 BOARD_DTB_OFFSET := 0x07c08000
 BOARD_KERNEL_TAGS_OFFSET := 0x07c08000
+
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
 BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_KERNEL_CONFIG := ruby_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ruby
+TARGET_KERNEL_CLANG_COMPILE := true 
 
-# Kernel - prebuilt
-TARGET_FORCE_PREBUILT_KERNEL := true
-ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(PREBUILT_PATH)/kernel
-TARGET_PREBUILT_DTB := $(PREBUILT_PATH)/dtb.img
-BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
-BOARD_INCLUDE_DTB_IN_BOOTIMG := 
-endif
+#BOARD_KERNEL_SEPARATED_DTBO := true 
+#BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
+# Encryption 
+ TW_INCLUDE_CRYPTO := true 
+ TW_INCLUDE_CRYPTO_FBE := true 
+ BOARD_USES_METADATA_PARTITION := true 
+ TW_INCLUDE_FBE_METADATA_DECRYPT := true 
+ PLATFORM_SECURITY_PATCH := 2099-12-31
+
+# fstab 
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 
 # Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
+BOARD_DTBOIMG_PARTITION_SIZE := 33554432
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 134217728
-BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
@@ -85,60 +106,49 @@ BOARD_SUPER_PARTITION_GROUPS := xiaomi_dynamic_partitions
 BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext vendor product
 BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
 
-# Platform
-TARGET_BOARD_PLATFORM := mt6877
-
-# Verified Boot
-BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
-
-# Assert 
-TARGET_OTA_ASSERT_DEVICE := ruby,rubypro
-  
-# Properties 
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop 
-  
-# fstab 
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
-
-# Metadata
-BOARD_USES_METADATA_PARTITION := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-# APEX 
- DEXPREOPT_GENERATE_APEX_IMAGE := true
+# Properties 
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop 
+  
+# Recovery 
+ BOARD_HAS_LARGE_FILESYSTEM := true 
+ BOARD_HAS_NO_SELECT_BUTTON := true 
+ BOARD_SUPPRESS_SECURE_ERASE := true 
+ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888" 
+ BOARD_USES_RECOVERY_AS_BOOT := true 
+ TARGET_NO_RECOVERY := true
 
-# Recovery
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+# Root 
+ BOARD_ROOT_EXTRA_FOLDERS += cust
 
-# System as Root
-BOARD_SUPPRESS_SECURE_ERASE := true
-
-# Encryption
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_FBE := true
+# System as root 
+ BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 
 # Treble
 BOARD_VNDK_VERSION := current
 
-ifneq ($(OF_HIDE_NOTCH),1) 
-     # Configure Status bar icons for regular TWRP builds only 
-     TW_CUSTOM_CLOCK_POS := 40 
-     TW_CUSTOM_CPU_POS := 605 
-     TW_STATUS_ICONS_ALIGN := center 
- endif
- 
-# TWRP Configuration
- TW_FRAMERATE := 60 
+# APEX 
+ DEXPREOPT_GENERATE_APEX_IMAGE := true
+
+# TWRP specific build flags 
+ #TW_DEVICE_VERSION := beta 
  TW_THEME := portrait_hdpi 
- TW_DEVICE_VERSION := Ruby_Norikhsan90 
- TW_EXTRA_LANGUAGES := true 
- TW_SCREEN_BLANK_ON_BOOT := true 
+ TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file 
+ TW_INCLUDE_RESETPROP := true 
+ TW_INCLUDE_REPACKTOOLS := true 
+ TWRP_INCLUDE_LOGCAT := true 
+ TARGET_USES_LOGD := true 
+ TW_EXCLUDE_DEFAULT_USB_INIT := true 
  TW_INPUT_BLACKLIST := "hbtp_vm" 
- TW_USE_TOOLBOX := true
- TW_INCLUDE_FASTBOOTD := true 
+ TW_EXTRA_LANGUAGES := true 
  TW_INCLUDE_NTFS_3G := true 
+ TW_MAX_BRIGHTNESS := 2047 
+ TW_DEFAULT_BRIGHTNESS := 1200 
+ TARGET_USES_MKE2FS := true 
+ TW_NO_SCREEN_BLANK := true 
+ TW_EXCLUDE_APEX := true 
  TW_NO_HAPTICS := true
 
 # Hack: prevent anti rollback
