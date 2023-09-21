c3640b6b6a397204caf214f6c20cebf67378bc42
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2023-09-21 16:59:42 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,22 +1,16 @@
- # 
+# 
  # Copyright (C) 2023 The Android Open Source Project 
  # 
  # SPDX-License-Identifier: Apache-2.0 
  # 
-
-# A/B 
+  
+ # A/B 
  # Boot control HAL 
  PRODUCT_PACKAGES += \ 
      android.hardware.boot@1.2-impl \ 
      android.hardware.boot@1.2-impl.recovery \ 
-     android.hardware.boot@1.2-service
+     android.hardware.boot@1.2-service 
   
- # FastbootD support 
- PRODUCT_PACKAGES += \ 
-     android.hardware.fastboot@1.1-impl-mock \ 
-     fastbootd 
-  
- # Update engine 
  PRODUCT_PACKAGES += \ 
      update_engine \ 
      update_engine_sideload \ 
@@ -25,25 +19,20 @@
  PRODUCT_PACKAGES_DEBUG += \ 
      update_engine_client 
   
- PRODUCT_PACKAGES += \ 
-     otapreopt_script \ 
-     checkpoint_gc  
-  
- # Virutal A/B 
- ENABLE_VIRTUAL_AB := true 
  AB_OTA_POSTINSTALL_CONFIG += \ 
      RUN_POSTINSTALL_system=true \ 
      POSTINSTALL_PATH_system=system/bin/otapreopt_script \ 
      FILESYSTEM_TYPE_system=erofs \ 
      POSTINSTALL_OPTIONAL_system=true 
   
- AB_OTA_POSTINSTALL_CONFIG += \ 
-     RUN_POSTINSTALL_vendor=true \ 
-     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \ 
-     FILESYSTEM_TYPE_vendor=erofs \ 
-     POSTINSTALL_OPTIONAL_vendor=true 
-
-# Keymaster 
+ PRODUCT_PACKAGES += \ 
+     otapreopt_script 
+  
+ PRODUCT_PACKAGES += \ 
+     android.hardware.fastboot@1.0-impl-mock \ 
+     fastbootd 
+  
+ # Keymaster 
  TARGET_RECOVERY_DEVICE_MODULES += \ 
      libkeymaster4 \ 
      libkeymaster41 \ 
