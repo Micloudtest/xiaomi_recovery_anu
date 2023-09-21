be005c57fd845157849de1359621d4107a1c6421
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2023-09-21 16:35:37 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -42,35 +42,4 @@
      POSTINSTALL_PATH_vendor=bin/checkpoint_gc \ 
      FILESYSTEM_TYPE_vendor=erofs \ 
      POSTINSTALL_OPTIONAL_vendor=true 
-  
- # TWRP - Specifics 
- TW_THEME                := portrait_hdpi 
- TW_DEFAULT_LANGUAGE     := en 
- TW_USE_TOOLBOX          := true 
- TW_INCLUDE_NTFS_3G      := true  
- TW_INCLUDE_REPACKTOOLS  := true 
- TW_INCLUDE_RESETPROP    := true 
- TW_INCLUDE_LIBRESETPROP := true 
- TW_MAX_BRIGHTNESS       := 2047 
- TW_EXTRA_LANGUAGES      := true 
- TW_DEFAULT_BRIGHTNESS   := 200 
- TW_EXCLUDE_APEX         := true 
- TW_INCLUDE_FASTBOOTD    := true 
- TWRP_INCLUDE_LOGCAT     := true 
-  
- # Set panel refresh rate to 90 Hertz for smoother experience 
- # Possible supported frequencies: 30, 60, 90, 120 
- TW_FRAMERATE := 90 
- TW_BATTERY_SYSFS_WAIT_SECONDS := 6 
- TW_EXCLUDE_DEFAULT_USB_INIT   := true 
-  
- # TWRP - Crypto 
- TW_INCLUDE_CRYPTO               := true 
- TW_INCLUDE_CRYPTO_FBE           := true 
- TW_INCLUDE_FBE_METADATA_DECRYPT := true 
- PLATFORM_VERSION                := 99.87.36 
- PLATFORM_VERSION_LAST_STABLE    := $(PLATFORM_VERSION) 
-  
- PLATFORM_SECURITY_PATCH := 2127-12-31 
- VENDOR_SECURITY_PATCH   := $(PLATFORM_SECURITY_PATCH) 
- BOOT_SECURITY_PATCH     := $(PLATFORM_SECURITY_PATCH)
+
