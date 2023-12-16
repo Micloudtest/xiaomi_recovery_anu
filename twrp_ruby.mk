15fd478e0de9786877f1e4631a8c5500c1825115
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2023-12-17 06:02:27 +0800

    Update twrp_ruby.mk

--- twrp_ruby.mk
+++ twrp_ruby.mk
@@ -1,16 +1,19 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit from device makefile.
+# Inherit from fleur device
 $(call inherit-product, device/xiaomi/ruby/device.mk)
 
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 ## Device identifier 
  PRODUCT_DEVICE := ruby 
@@ -19,4 +22,3 @@ $(call inherit-product, device/xiaomi/ruby/device.mk)
  PRODUCT_MODEL := Redmi Note 12 Pro 5G
  PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
